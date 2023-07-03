5a4049509dc017f2d7408eaacaeb6a6406557400
Sahid Rahman <sahidrahman404@gmail.com>
2023-07-03 19:20:59 +0700

    add EquipmentStorer

---
+++ internal/database/equipment.go
@@ -0,0 +1,67 @@
+package database
+
+import (
+	"context"
+
+	"github.com/sahidrahman404/gigachad-api/ent"
+	"github.com/sahidrahman404/gigachad-api/internal/types"
+)
+
+type EquipmentStorer interface {
+	Insert(ctx context.Context, eq *types.Equipment) error
+	Get(ctx context.Context, id string) (*types.Equipment, error)
+	Update(ctx context.Context, eq *types.Equipment) error
+	GetAll(ctx context.Context) (*[]types.Equipment, error)
+}
+
+type EquipmentStore struct {
+	Client *ent.Client
+}
+
+func NewEquipmentStore(c *ent.Client) *EquipmentStore {
+	return &EquipmentStore{
+		Client: c,
+	}
+}
+
+func (e *EquipmentStore) Insert(ctx context.Context, eq *types.Equipment) error {
+	equipment, err := e.Client.Equipment.Create().SetName(eq.Ent.Name).SetImage(eq.Ent.Image).Save(ctx)
+	if err != nil {
+		return err
+	}
+	eq.Ent.ID = equipment.ID
+	return nil
+}
+
+func (e *EquipmentStore) Get(ctx context.Context, id string) (*types.Equipment, error) {
+	equipment, err := e.Client.Equipment.Get(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &types.Equipment{
+		Ent: equipment,
+	}, err
+}
+
+func (e *EquipmentStore) Update(ctx context.Context, eq *types.Equipment) error {
+	_, err := e.Client.Equipment.UpdateOneID(eq.Ent.ID).
+		SetName(eq.Ent.Name).SetImage(eq.Ent.Image).Save(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *EquipmentStore) GetAll(ctx context.Context) (*[]types.Equipment, error) {
+	equipment, err := e.Client.Equipment.Query().All(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var eq *[]types.Equipment
+	for _, v := range equipment {
+		*eq = append(*eq, types.Equipment{
+			Ent: v,
+		})
+	}
+	return eq, err
+}
