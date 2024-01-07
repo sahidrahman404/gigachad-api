789fef28e8396cd559ffc3050aefdb5c120a54cb
Sahid Rahman <sahidrahman404@gmail.com>
2024-01-07 22:12:12 +0700

    update

--- ent/schema/schematype/schematype.go
+++ ent/schema/schematype/schematype.go
@@ -1,10 +1,50 @@
 package schematype
 
+import "strconv"
+
 type Set struct {
 	Reps     *int    `json:"reps,omitempty"`
 	Kg       *int    `json:"kg,omitempty"`
 	Duration *string `json:"duration,omitempty"`
 	Km       *int    `json:"km,omitempty"`
+}
+
+type SetDisplay struct {
+	Kg       string `json:"kg,omitempty"`
+	Reps     string `json:"reps,omitempty"`
+	Duration string `json:"duration,omitempty"`
+	Km       string `json:"km,omitempty"`
+}
+
+func (s Set) Display() SetDisplay {
+	sd := SetDisplay{}
+	empty := "-"
+	if s.Reps == nil {
+		sd.Reps = empty
+	}
+	if s.Kg == nil {
+		sd.Kg = empty
+	}
+	if s.Duration == nil {
+		sd.Duration = empty
+	}
+	if s.Km == nil {
+		sd.Km = empty
+	}
+
+	if s.Reps != nil {
+		sd.Reps = strconv.Itoa(*s.Reps)
+	}
+	if s.Kg != nil {
+		sd.Kg = strconv.Itoa(*s.Kg)
+	}
+	if s.Duration != nil {
+		sd.Duration = *s.Duration
+	}
+	if s.Km != nil {
+		sd.Km = strconv.Itoa(*s.Km)
+	}
+	return sd
 }
 
 type Style struct {
